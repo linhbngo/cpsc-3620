5b21b02347e15efdb35b51ffd8d73e674bbe6ec2
Linh Ngo <lngo@clemson.edu>
2017-02-06 08:55:11 -0500

    rearrange contents of codes

---
+++ codes/mpi4py/gethostname.py
@@ -0,0 +1,7 @@
+#hello.py
+from mpi4py import MPI
+import socket
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+print ("hello world from process %s running on host %s" % (rank, socket.gethostname()))
